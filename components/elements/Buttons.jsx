1a3f0f1ec15053b4abace5eec11d0c2a02f5089f
Михаил Хорошев <khoroshev_mikhail@Noutbuk-Mihail.local>
2025-05-07 08:54:07 +0300

    Правки

--- components/elements/Buttons.jsx
+++ components/elements/Buttons.jsx
@@ -12,7 +12,7 @@ export default function Buttons({ blue, green }){
                 WHITEPAPER
             </a>
             <a href="https://raydium.io/swap/?inputMint=sol&outputMint=2hJBZzXhuX8XguKfQQCiwMC9jPfKpLtygYXEFZjAKR44" className={`${RAYDIS} cursor-pointer aspect-[183/63] px-3.5 w-[125px] sm:w-[140px] md:w-[160px] lg:w-[183px] md:min-w-[183px] flex flex-col justify-center bg-cover bg-[url('/img/elements/buttons/blue.png')] bg-no-repeat bg-center pt-2.5 text-[13px] md:text-[15px] lg:text-[17px] xl:text-[20px] text-center text-white`}>
-                PRESALE
+                RAYDIUM
             </a>
 
         </div>
